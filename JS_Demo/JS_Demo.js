f3fe676cd1648abb9f86c4de7b8dbbaa6128ea17
James Hinson <octobersith@outlook.com>
2022-07-24 01:18:40 -0400

    Updated gif

--- JS_Demo/JS_Demo.js
+++ JS_Demo/JS_Demo.js
@@ -1,5 +1,19 @@
 //Basic variable declaration
 let x = 5
-let y = 5
+let y = 3
 console.log(x+y)
+console.log(5/ 3)
+console.log(5 % 3)
+
+//Powers
+console.log(5 ** 2)
+console.log(5 ** -2)
+
+//NaN and Infinity
+console.log(0/0)
+console.log(1/0)
+
+
+
+
 
