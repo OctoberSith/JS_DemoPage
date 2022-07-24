3015a11adab34ef174fff2ff38f68bbf2a7c3566
James Hinson <octobersith@outlook.com>
2022-07-24 02:28:02 -0400

    updated

--- JS_Demo/JS_Demo.js
+++ JS_Demo/JS_Demo.js
@@ -13,7 +13,13 @@ console.log(5 ** -2)
 console.log(0/0)
 console.log(1/0)
 
-
-
-
+//Password Prompt
+// const password = "sid"
+// const passwordInput = prompt("Please Enter Your Password")
+// if(password === passwordInput){
+//     alert("Your Password is correct!")
+// }
+// else{
+//     alert("Your Password is wrong dummy!")
+// }
 
